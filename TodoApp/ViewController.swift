5ed39c2603e3920fb7d6318fc0e77fbc6dfbb236
jinyongp <dev.jinyongp@gmail.com>
2023-07-31 16:08:55 +0900

    feat: title label 애니메이션 추가

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let centerY = self.view.center.y - titleLabel.bounds.height - 20
+        let scaling = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        let translation = CGAffineTransform(translationX: 0, y: centerY)
+        self.titleLabel.transform = scaling.concatenating(translation)
+
+        UIView.animate(
+            withDuration: 1.0,
+            delay: 0.5,
+            usingSpringWithDamping: 100,
+            initialSpringVelocity: 10
+        ) {
+            self.titleLabel.transform = .identity
+        }
     }
-
-
 }
-
