f1ecd3fc58e845cd551acc38e4db448e58b9aa1f
jinyongp <dev.jinyongp@gmail.com>
2023-08-02 16:39:03 +0900

    feat: 화면 구성

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -1,28 +1,19 @@
-//
-//  ViewController.swift
-//  TodoApp
-//
-//  Created by Jinyong Park on 2023/07/31.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var completesButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
 
-        let centerY = self.view.center.y - titleLabel.bounds.height - 20
-        let scaling = CGAffineTransform(scaleX: 2.0, y: 2.0)
-        let translation = CGAffineTransform(translationX: 0, y: centerY)
-        self.titleLabel.transform = scaling.concatenating(translation)
+        titleLabel.transform = CGAffineTransform(translationX: 0, y: 200)
 
         UIView.animate(
             withDuration: 1.0,
-            delay: 0.5,
-            usingSpringWithDamping: 100,
+            delay: 0.2,
+            usingSpringWithDamping: 1.0,
             initialSpringVelocity: 10
         ) {
             self.titleLabel.transform = .identity
